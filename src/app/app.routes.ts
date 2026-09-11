544cf0a936b2cce15fc9f4db807f9dce6ceee5d9
unknown <joni.peri72@gmail.com>
2026-09-10 21:07:20 -0300

    cambios pequenos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './componentes/inicio/inicio';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    {
+        path: '',
+        redirectTo: 'inicio',
+        pathMatch: 'full'
+    },
+    {
+        path: 'inicio',
+        component: Inicio
+    },
+    {
+        path: 'cartelera',
+        loadComponent: () => import('./componentes/cartelera/cartelera').then(m => m.Cartelera) 
+    }
+
+];
