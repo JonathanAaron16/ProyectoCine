10a3f3450a7e812d66cd54ba2389452ff6e756e1
Jonathan Perez <japerez@dominico.fra.utn.edu.ar>
2026-09-17 10:39:33 -0300

    Eliminar archivo subido por error

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://iydzyvriktfdcaxvqhpl.supabase.co',
-  supabasePublishableKey: 'sb_publishable_tJO9TH89J4OAOxKasZT6Wg_bbiLHf2i',
-  //Databasepassword:'NZXTP1qJr8NeggLl'
-}
